98195899fc390d3afc0ea99ee7f06fc253201708
Nathan Nieuwenhuizen <nathan-san@live.nl>
2020-02-01 22:18:25 +0100

    adding ring art

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rotationDirection;
+
+    void Update()
+    {
+        transform.Rotate(rotationDirection);
+    }
+}
