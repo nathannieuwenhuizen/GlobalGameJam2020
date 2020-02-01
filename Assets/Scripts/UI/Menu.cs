4866241f1613c1edc6bb4c7c99ba90091b0193ba
Nathan Nieuwenhuizen <nathan-san@live.nl>
2020-02-01 17:30:45 +0100

    fix dotted line and menu settings

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private TextMeshProUGUI playerText;
+
+    [SerializeField]
+    private Slider playerSlider;
+
+    [SerializeField]
+    private TextMeshProUGUI lapsText;
+
+    [SerializeField]
+    private Slider lapsSlider;
+
     void Start()
     {
-        
+        lapsSlider.value = Globals.AMOUNT_LAPS;
+        playerSlider.value = Globals.AMOUNT_PLAYERS;
+        playerText.text = "player: " + Globals.AMOUNT_PLAYERS;
+        lapsText.text = "laps: " + Globals.AMOUNT_LAPS;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateSettings()
     {
-        
+        Globals.AMOUNT_LAPS = (int)lapsSlider.value;
+        Globals.AMOUNT_PLAYERS = (int)playerSlider.value;
+
+        playerText.text = "player: " + Globals.AMOUNT_PLAYERS;
+        lapsText.text = "laps: " + Globals.AMOUNT_LAPS;
+
     }
 }
