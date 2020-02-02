2daffe30e377ff825b6a1c37474f2d3f4d5e58ca
Nathan Nieuwenhuizen <nathan-san@live.nl>
2020-02-02 10:57:04 +0100

    pause bug fixed

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,19 +5,23 @@ public class SceneLoader : MonoBehaviour {
     
     public void LoadNewScene(string scene)
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(scene);
     }
 
     public void LoadNextScene()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void ReloadScene()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void LadSceneByBuildIndex(int buildIndex)
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(buildIndex);
 
     }
